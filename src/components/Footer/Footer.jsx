054bb9bd9f04d5f08a1c7d36d8cf4c89055b4595
MS Rahman <msrahman048gmail.com>
2024-03-23 19:42:09 +0600

    md-47-2 completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 
 const Footer = () => {
+    
     return (
         <footer className="footer p-10 bg-base-200 text-base-content">
         <aside>
--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,22 @@ import {
 import './index.css'
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
+import Appliedjobs from './components/Appliedjobs/Appliedjobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
     children:[
     {
       path: '/',
-      element:<Home></Home>,
+      element:<Home></Home>
+    },
+    {
+      path: '/applied',
+      element:<Appliedjobs></Appliedjobs>
     }
   ]
   },
