d03f0642dd25eddc684d46f6d7eb82d11acccd89
MS Rahman <msrahman048gmail.com>
2024-03-24 16:35:30 +0600

    md-47-6 completed

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,7 +1,8 @@
 import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineDollar } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const Job = ({job}) => {
-    const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary} =job;
+    const {id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary} =job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Shoes" /></figure>
@@ -12,12 +13,14 @@ const Job = ({job}) => {
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE]">{job_type}</button>
                 </div>
-                <div className="card-actions mt-4">
-                <button className="btn bg-[#7E90FE]">View Details</button>
-                </div>
                 <div className="flex gap-4 items-center text-2xl mt-4">
                     <h2 className="flex gap-2 items-center"><CiLocationOn></CiLocationOn>{location}</h2>
                     <h2 className="flex gap-2 items-center"><AiOutlineDollar></AiOutlineDollar>{salary}</h2>
+                </div>
+                <div className="card-actions mt-4">
+                <Link to={`/job/${id}`}>
+                    <button className="btn bg-[#7E90FE]">View Details</button>
+                </Link>
                 </div>
             </div>
         </div>
